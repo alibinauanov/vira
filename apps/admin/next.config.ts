876376c76c430a4af191b2a74c3a34b250b2cbef
alibinauanov <alibinauanov371@gmail.com>
2025-12-24 15:13:36 +0500

    admin deploy fix #13

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname, "../../"),
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
 };
 
 export default nextConfig;
